32c5b5cde0e88353925f4a3f456d512022ffb2e2
husakov <samhusak@hotmail.com>
2018-10-23 13:55:44 +0200

    Project created,router setuped,working on dashboard and login finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+
+const appRoutes: Routes = [
+  {path: 'dashboard' , component:
+  DashboardComponent
+  },
+  {path: 'login' , component:
+  LoginComponent
+  }
+
+];
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+      ),
     BrowserModule,
     AppRoutingModule
   ],
