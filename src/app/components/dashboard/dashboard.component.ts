265f8d3d7d9a753f599a7f1d76c3c588563c87d6
husakov <samhusak@hotmail.com>
2018-10-24 21:30:42 +0200

    Deleted unused code

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,14 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 
-angular.module('demo', [])
-  .controller('Hello', function($scope, $http) {
-    $http.get('f4HYBYdeqTZ7KNb').
-    then(function(response) {
-      $scope.greeting = response.data;
-    });
-  });
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
